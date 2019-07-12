7e863ab4a20ed7b8efa19ca471f97ea5cbcc14bb
daniel-carter <52665500+daniel-carter@users.noreply.github.com>
2019-07-12 11:09:56 -0400

    Day 2 Questions
    Attached are my programs written for the day 2 questions.
    Thank you.

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=113;
+
+
+		if(num%2==0)
+			System.out.println("Even Number");
+		else
+			System.out.println("Odd Number");	
+		
+		
+	}
+
+}
---
+++ VowelOrConsonant.java
@@ -0,0 +1,40 @@
+public class VowelOrConsonant {
+public static void main(String[] arg){
+char ch='b';
+switch(ch)
+{
+case 'a':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'e':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'i':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'o':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'u':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'A':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'E':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'I':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'O':
+	System.out.println("Letter a is Vowel");
+	break;
+case 'U':
+	System.out.println("Letter a is Vowel");
+	break;
+default:
+	System.out.println("Letter is a Consonant");
+	}
+}
+}
